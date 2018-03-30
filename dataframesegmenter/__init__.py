337c9d06a363ceb824380403865e3b425d681738
Xavier Tolza <tolza.xavier@gmail.com>
2018-03-30 11:56:27 +0200

    added a tool function available at import

--- dataframesegmenter/__init__.py
+++ dataframesegmenter/__init__.py
@@ -1,3 +1,4 @@
 from dataframesegmenter.segmenter import Segmenter
+from dataframesegmenter.tools import segment_dataframe_per_column
 
-__all__ = ["Segmenter"]
+__all__ = ["Segmenter", "segment_dataframe_per_column"]
